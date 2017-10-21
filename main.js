5b74c5cba2c21f54a157d7215c8099886ae1c439
ProCxxx <proxturt@gmail.com>
2017-10-22 00:32:49 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,8 @@
 var i=1;
 function a(){
-setInterval(function(){ 
-
-	var c=document.getElementById("canv");
-	var ctx=c.getContext("2d");
-	c.width=961;
-	c.height=1281;
-	
+setInterval(function(){ 	
 	var img = document.getElementById("img");
-	
-	var x = "img/img"+i+".jpg";
 	img.src="img/img"+i+".jpg";
-	ctx.drawImage(img, 0,0, c.width, c.height);
-	console.log(x+" "+i);
 	i=Math.floor(Math.random()*47 +1);
-	//i++;
-	//if(i>48){i=1;}
 },1000);
 }
